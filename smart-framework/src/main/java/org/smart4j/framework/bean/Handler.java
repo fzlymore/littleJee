c0cd34a93863f2c292f1eb15321f3cc119f8b6ea
fzzl <464774994@qq.com>
2017-11-27 18:32:11 +0800

    bean管理

---
+++ smart-framework/src/main/java/org/smart4j/framework/bean/Handler.java
@@ -0,0 +1,20 @@
+package org.smart4j.framework.bean;
+
+import java.lang.reflect.Method;
+
+/**
+ * Created by wh1te on 2017/11/27.
+ * 封装Action信息
+ */
+public class Handler {
+    private Method actionMethod;
+    /**
+     * controller类
+     */
+    private Class<?> controllerClass;
+
+    public Handler(Class<?>controllerClass , Method actionMethod){
+        this.controllerClass = controllerClass;
+        this.actionMethod = actionMethod;
+    }
+}
